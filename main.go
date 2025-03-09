591200cb01b52840e48f4ba333bc99b54029ef1a
Alex Idowu <alexidowu25@gmail.com>
2025-03-09 11:13:58 +0000

    simulate resource allocation

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func cpuWork(wg *sync.WaitGroup, duration time.Duration) {
 	defer wg.Done()
 	start := time.Now()
-	end := start.Add(duration)
+	end := start.Add(duration / 2) // Reduce CPU work duration
 	for time.Now().Before(end) {
 		// Simulate CPU work
 		_ = 1 + 1 // Simple operation to keep CPU busy
@@ -19,7 +19,7 @@ func cpuWork(wg *sync.WaitGroup, duration time.Duration) {
 
 func memoryWork(wg *sync.WaitGroup, duration time.Duration) {
 	defer wg.Done()
-	mem := make([]byte, 2*1024*1024*1024) // Allocate 2 GB of memory
+	mem := make([]byte, 4*1024*1024*1024) // Allocate 4 GB of memory
 	start := time.Now()
 	end := start.Add(duration)
 	for time.Now().Before(end) {
@@ -32,13 +32,13 @@ func memoryWork(wg *sync.WaitGroup, duration time.Duration) {
 }
 
 func main() {
-	runtime.GOMAXPROCS(2) // Use 2 CPU cores
+	runtime.GOMAXPROCS(1) // Use 1 CPU core
 	var wg sync.WaitGroup
 	duration := 10 * time.Second // Test duration
 
 	fmt.Println("Starting CPU and memory work")
 
-	for i := 0; i < 105; i++ { // Run the test 5 times
+	for i := 0; i < 5; i++ { // Run the test 5 times
 		fmt.Printf("Iteration %d: Starting CPU and memory work\n", i+1)
 		wg.Add(2)
 		go cpuWork(&wg, duration)
