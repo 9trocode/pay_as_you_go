1d28f18b3ee5190001c05e2ffa8c4334bef6931f
Alex Idowu <alexidowu25@gmail.com>
2025-03-09 12:18:38 +0000

    simulate resource allocation

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"log/syslog"
 	"math/rand"
 	"runtime"
 	"time"
 )
 
 func main() {
+	// Set up syslog logging
+	sysLogger, err := syslog.New(syslog.LOG_INFO, "constant_load")
+	if err != nil {
+		log.Fatalf("Failed to set up syslog: %v", err)
+	}
+	log.SetOutput(sysLogger)
+	log.Println("Starting constant load program")
+
 	// Allocate memory to reach 512 MiB
 	mem := make([]byte, 512*1024*1024)
 	for i := range mem {
@@ -17,6 +27,7 @@ func main() {
 	// Display memory usage
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
+	log.Printf("Allocated memory: %d MiB\n", memStats.Alloc/(1024*1024))
 	fmt.Printf("Allocated memory: %d MiB\n", memStats.Alloc/(1024*1024))
 
 	// Create load to use around 0.3 CPU
@@ -31,6 +42,7 @@ func main() {
 			for time.Since(start) < 30*time.Millisecond {
 				_ = rand.Float64() * rand.Float64() // Simple computation
 			}
+			log.Println("Performed computation cycle")
 		}
 	}
 }
